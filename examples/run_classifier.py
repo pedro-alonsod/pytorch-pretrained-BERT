3919eb1bae18d119d4d74a45d3b88b52206700f1
Pedro Alonso <pedro.alonsod@gmail.com>
2019-06-14 10:39:20 +0200

    running code
    run at the moment

--- examples/run_classifier.py
+++ examples/run_classifier.py
@@ -269,6 +269,38 @@ class StsbProcessor(DataProcessor):
                 InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
         return examples
 
+class RedditProcess(DataProcessor):
+    # class StsbProcessor(DataProcessor):
+    """Processor for the Reddit data set (GLUE version)."""
+
+    def get_train_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "train.tsv")), "train")
+
+    def get_dev_examples(self, data_dir):
+        """See base class."""
+        return self._create_examples(
+            self._read_tsv(os.path.join(data_dir, "dev.tsv")), "dev")
+
+    def get_labels(self):
+        """See base class."""
+        return [None]
+
+    def _create_examples(self, lines, set_type):
+        """Creates examples for the training and dev sets."""
+        examples = []
+        for (i, line) in enumerate(lines):
+            if i == 0:
+                continue
+            guid = "%s-0" % (set_type)
+            text_a = line[0]
+            text_b = line[1]
+            label = line[-1]
+            examples.append(
+                InputExample(guid=guid, text_a=text_a, text_b=text_b, label=label))
+        return examples
+
 
 class QqpProcessor(DataProcessor):
     """Processor for the QQP data set (GLUE version)."""
@@ -671,6 +703,7 @@ def main():
         "qnli": QnliProcessor,
         "rte": RteProcessor,
         "wnli": WnliProcessor,
+        "reddit": RedditProcess,
     }
 
     output_modes = {
@@ -683,6 +716,7 @@ def main():
         "qnli": "classification",
         "rte": "classification",
         "wnli": "classification",
+        "reddit": "regression",
     }
 
     if args.local_rank == -1 or args.no_cuda:
